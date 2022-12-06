1729dd9477075e52b877fc70d15ebad5d17ece15
timur-muminov1 <biigbeenni@gmail.com>
2022-12-06 18:35:01 +0300

    case bug demonstration

--- app/src/main/java/com/myapplication3/sample/MainActivity.kt
+++ app/src/main/java/com/myapplication3/sample/MainActivity.kt
@@ -2,17 +2,24 @@ package com.myapplication3.sample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.myapplication3.R
+import dev.icerock.moko.permissions.Permission
 import dev.icerock.moko.permissions.PermissionsController
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         val permissionsController = PermissionsController(applicationContext = this)
 
+        permissionsController.bind(lifecycle,supportFragmentManager)
 
-
+        with(lifecycleScope){
+            launch{  permissionsController.providePermission(Permission.STORAGE) }
+            launch{  permissionsController.providePermission(Permission.CAMERA) }
+        }
+        setContentView(R.layout.activity_main)
     }
 }
